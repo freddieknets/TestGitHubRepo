8b2cf1e1fcac02ce61bfe7e36d2bc68002474483
Frederik Van der Veken <frederik@cern.ch>
2023-06-14 13:38:16 +0200

    Fixed version script.

--- version.sh
+++ version.sh
@@ -7,17 +7,36 @@ then
     exit 1
 fi
 
+branch=$(git status | head -1 | awk '{print $NF}')
+if [[ "$branch" == "main" ]]
+then
+    echo "Error: this script cannot be ran on the main branch."
+    echo "Make a release branch and make the new release from there."
+    exit 1
+fi
+
+# Update version in temporary branch
 poetry version $1
 new_ver=$( poetry version | awk '{print $2;}' )
-sed -i "s/\(__version__ =\).*/\1 '"${new_ver}"'/"         TestGitHubRepo/__init__.py
+sed -i "s/\(__version__ =\).*/\1 '"${new_ver}"'/"         testgithubrepo/__init__.py
 sed -i "s/\(assert __version__ ==\).*/\1 '"${new_ver}"'/" tests/test_version.py
 git reset
-git add pyproject.toml TestGitHubRepo/__init__.py tests/test_version.py
+git add pyproject.toml testgithubrepo/__init__.py tests/test_version.py
 git commit -m "Updated version number to v"${new_ver}"."
 git push
+
+# Make and accept pull request
+gh pr create --base main --title "Release "${new_ver} --fill
+git switch main
+git pull
+PR=$( gh pr list --head $branch | tail -1 | awk '{print $1;}' )
+gh pr merge ${PR} --merge --admin --delete-branch
+
+# Make a tag
 git tag v${new_ver}
 git push origin v${new_ver}
 
+# Draft release notes
 curl \
   -X POST \
   -H "Accept: application/vnd.github+json" \
@@ -25,4 +44,5 @@ curl \
   https://api.github.com/repos/freddieknets/TestGitHubRepo/releases \
   -d '{"tag_name":"v'${new_ver}'","target_commitish":"main","name":"TestGitHubRepo release '${new_ver}'","body":"","draft":true,"prerelease":false,"generate_release_notes":true}'
 
-poetry publish --build
+# Build release and publish to PyPi
+#poetry publish --build
