8ddbe30785f3b6787e5942e474e4c686a33f583d
Frederik Van der Veken <frederik@cern.ch>
2023-06-14 10:23:43 +0200

    Initial commit

---
+++ version.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+    echo "This script needs exactly one argument: the new version number or a bump."
+    echo "The latter can be: patch, minor, major, prepatch, preminor, premajor, prerelease."
+    exit 1
+fi
+
+poetry version $1
+new_ver=$( poetry version | awk '{print $2;}' )
+sed -i "s/\(__version__ =\).*/\1 '"${new_ver}"'/"         TestGitHubRepo/__init__.py
+sed -i "s/\(assert __version__ ==\).*/\1 '"${new_ver}"'/" tests/test_version.py
+git reset
+git add pyproject.toml TestGitHubRepo/__init__.py tests/test_version.py
+git commit -m "Updated version number to v"${new_ver}"."
+git push
+git tag v${new_ver}
+git push origin v${new_ver}
+
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer "$(cat ../github_token) \
+  https://api.github.com/repos/freddieknets/TestGitHubRepo/releases \
+  -d '{"tag_name":"v'${new_ver}'","target_commitish":"main","name":"TestGitHubRepo release '${new_ver}'","body":"","draft":true,"prerelease":false,"generate_release_notes":true}'
+
+poetry publish --build
